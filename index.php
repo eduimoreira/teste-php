3faf66df163c888b320c5988b73065c917e078d8
eduimoreira <eduimoreira@hotmail.com>
2022-08-23 12:37:31 -0300

    conexao com bd adicionada

--- index.php
+++ index.php
@@ -1,3 +1,54 @@
+<?php
+
+    include('conexao.php');
+    if(isset($_POST['usuario']) || isset($_POST['senha'])   ){
+
+
+
+        if (strlen($_POST['usuario'])==0) {
+             echo "Preencha a disgrassa do usuario";   
+            
+        }elseif (strlen($_POST['senha'])==0) {
+            echo "Preencha a disgrassa da senha";
+
+
+
+            
+        }else{
+
+            $usuario = $mysqli->real_escape_string($_POST['usuario']);
+            $senha = $mysqli->real_escape_string($_POST['senha']);
+
+            $sql_code = "SELECT * FROM usuarios WHERE user= '$usuario' AND senha= '$senha' ";
+            $sql_query = $mysqli->query($sql_code) or die("Deu pau no banco: ". $mysqli->error);
+
+            $quantidade = $sql_query->num_rows;
+
+
+            if ($quantidade==1) {
+
+                $usuario= $sql_query->fetch_assoc();
+
+                if(!isset($_SESSION)){
+                      session_start();  
+
+                }
+                $_SESSION['id']= $usuario['id'];
+                $_SESSION['user']= $usuario['usuario'];
+                header('Location: painel.php');
+
+                }
+
+                 else{
+
+            echo "Falha ao logar! email ou senha incorreto!";
+        }
+
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
